1ae31abc30dbdbf805fd760dc41a39ead9703499
Marcos Rogerio Ferreira <2413666@ifms.edu.br>
2023-07-31 22:42:23 -0400

    Criando o projeto

---
+++ src/main/java/br/com/anpede/resources/AssociadoResource.java
@@ -0,0 +1,33 @@
+package br.com.anpede.resources;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.anpede.entities.Associado;
+
+@RestController
+@RequestMapping(value = "/associados")
+public class AssociadoResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Associado>> findAll() {
+		List<Associado> list = new ArrayList<>();
+		list.add(new Associado(1L, "Angela Maria", "123.456.789-78",
+				LocalDate.parse("1994-10-21", DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+				"(67) 98888-5555", "angela@gmail.com", "Av. Caarapo, 12, Centro"));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
+
+
+
+
+
