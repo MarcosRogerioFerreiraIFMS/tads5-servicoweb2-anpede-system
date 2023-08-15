d7cc1598a001c19a1c4bcfa3ec95791211edb696
Marcos Rogerio Ferreira <2413666@ifms.edu.br>
2023-08-14 21:37:35 -0400

    Camada de POST, PUT, DELETE

--- src/main/java/br/com/anpede/resources/AssociadoResource.java
+++ src/main/java/br/com/anpede/resources/AssociadoResource.java
@@ -1,13 +1,19 @@
 package br.com.anpede.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.anpede.dto.AssociadoDTO;
 import br.com.anpede.entities.Associado;
@@ -31,8 +37,38 @@ public class AssociadoResource {
 		AssociadoDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
+	
+	@PostMapping
+	public ResponseEntity<AssociadoDTO> insert(@RequestBody AssociadoDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(dto.getId())
+					.toUri();
+		return ResponseEntity.created(uri).body(null);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<AssociadoDTO> update(
+			@PathVariable Long id,
+			@RequestBody AssociadoDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
+
+
+
+
+
 
 
 
--- src/main/java/br/com/anpede/services/AssociadoService.java
+++ src/main/java/br/com/anpede/services/AssociadoService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.anpede.dto.AssociadoDTO;
 import br.com.anpede.entities.Associado;
 import br.com.anpede.repositories.AssociadoRepository;
+import br.com.anpede.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class AssociadoService {
@@ -27,10 +30,63 @@ public class AssociadoService {
 	@Transactional(readOnly = true)
 	public AssociadoDTO findById(Long id) {
 		Optional<Associado> obj = repository.findById(id);
-		Associado entity = obj.get();
+		
+		Associado entity = obj.orElseThrow(() -> new ResourceNotFoundException("O registro solicitado não foi localizado."));
 		return new AssociadoDTO(entity);		
 	}
+
+	@Transactional
+	public AssociadoDTO insert(AssociadoDTO dto) {
+		Associado entity = new Associado();
+		entity.setNome(dto.getNome());
+		entity.setCPF(dto.getCPF());
+		entity.setDataNascimento(dto.getDataNascimento());
+		entity.setTelefone(dto.getTelefone());
+		entity.setEmail(dto.getEmail());
+		entity.setEndereco(dto.getEndereco());
+		
+		entity = repository.save(entity);
+
+		return new AssociadoDTO(entity);
+	}
+
+	@Transactional
+	public AssociadoDTO update(Long id, AssociadoDTO dto) {
+		
+		try {
+			Associado entity = repository.getReferenceById(id);
+			
+			entity.setNome(dto.getNome());
+			entity.setCPF(dto.getCPF());
+			entity.setDataNascimento(dto.getDataNascimento());
+			entity.setTelefone(dto.getTelefone());
+			entity.setEmail(dto.getEmail());
+			entity.setEndereco(dto.getEndereco());
+			
+			entity = repository.save(entity);
+	
+			return new AssociadoDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(
+					"O recurso com o ID "+id+" não foi localizado");
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(
+					"O recurso com o ID "+id+" não foi localizado");
+		}
+	}
 }
+
+
+
+
+
+
 
 
 
